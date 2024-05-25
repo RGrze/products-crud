d50e0f7da044dc397c01fdb31b12e94ab5c18cb5
rgrzesiakowski <roman.grzesiakowski@dronehub.ai>
2024-05-25 22:40:44 +0200

    Add get_user dependency

---
+++ app/dependencies/auth.py
@@ -0,0 +1,24 @@
+from typing import Annotated
+
+from fastapi import Security, HTTPException, status, Depends
+from fastapi.security import APIKeyHeader
+from sqlalchemy.orm import Session
+
+from app.api.v1.services.auth import get_user_from_api_key
+from app.database.db import get_db
+
+
+api_key_header = APIKeyHeader(name="X-API-Key")
+
+
+def get_user(
+    api_key: Annotated[str, Security(api_key_header)],
+    db: Annotated[Session, Depends(get_db)]
+):
+    user = get_user_from_api_key(db, api_key)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid API key"
+        )
+    return user
