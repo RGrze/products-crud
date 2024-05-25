4b46c68a9beb1ff3ae49ef5dd4af1e3b7613befd
rgrzesiakowski <roman.grzesiakowski@dronehub.ai>
2024-05-25 23:23:51 +0200

    Add implementation of get_user_from_api_key

--- app/api/v1/services/auth.py
+++ app/api/v1/services/auth.py
@@ -1,7 +1,12 @@
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.database.models.users import ApiKey, User
 
 
 def get_user_from_api_key(db: Session, api_key: str) -> User | None:
-    ...
+    stmt = select(ApiKey).where(ApiKey.key == api_key)
+    api_key = db.execute(stmt).scalar()
+    if api_key is None:
+        return None
+    return api_key.user
